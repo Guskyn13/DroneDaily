6e8b4a36f4b8be38838104e2f141abdaf05cdfb1
Nathan <n.gusky88@gmail.com>
2021-07-08 17:42:13 -0600

    home css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import HeaderCss from './Header.module.css'
 
 const Header = () => {
      return (
-          <div>
-               <h1>Header</h1>
-          </div>
+          <header className={HeaderCss.login_signup}>
+
+               <nav>
+                    <Link className={HeaderCss.nav}>Home</Link>
+                    <Link className={HeaderCss.nav}>About</Link>
+                    <Link className={HeaderCss.nav}>Contact</Link>
+               </nav>
+               <div className={HeaderCss.btns}>
+                    <ul>
+                         <li><Link to="/login">Login</Link></li>
+                         <li><Link to="/signup" className={HeaderCss.signup}>Sign Up</Link></li>
+                    </ul>
+               </div>
+          </header>
      )
 }
 
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import HomeCss from './Home.module.css'
 
 //Components
 import Header from '../../Components/Header/Header'
+
+//Images
+import Logo from '../../Images/Logo.png'
 
 export default function Home() {
      return (
           <div>
                <Header />
-               <h1>Home</h1>
+               <div className={HomeCss.main_container}>
+                    <div className={HomeCss.data}>
+                         <img src={Logo} className={HomeCss.logo_img} alt="drone daily logo" />
+                         <div className={HomeCss.title}>
+                              <h1>Drone <span style={{ color: "hotpink" }}>Daily</span></h1>
+                         </div>
+                    </div>
+               </div>
           </div>
      )
 }
