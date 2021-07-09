af1f81802fd9ccb118b63ba1fc67c5b1e0d50a99
Nathan <n.gusky88@gmail.com>
2021-07-08 19:21:20 -0600

    signup done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -4,6 +4,9 @@ import { Link, useHistory } from 'react-router-dom'
 import FirebaseContext from '../../Context/Firebase'
 import { doesUsernameExist } from '../../UserFunctions/UserFunctions'
 
+//Images
+import Logo from '../../Images/Logo.png'
+
 export default function SignUp() {
      const history = useHistory()
      const { firebase } = useContext(FirebaseContext)
@@ -58,7 +61,40 @@ export default function SignUp() {
 
      return (
           <div className={SignupCSS.signup}>
-               <h1>Sign Up</h1>
+
+               <img src={Logo} className={SignupCSS.imgLogo} alt="logo" />
+
+               {error && <div className={SignupCSS.error} style={{ color: "red" }}>{error}</div>}
+
+               <form className={SignupCSS.signup_card} onSubmit={handleSignup} id="signup_form" method='post' >
+                    <h2 className={SignupCSS.signup_h2}>Sign Up</h2>
+
+                    <div className={SignupCSS.inputBox}>
+                         <input type="text" id="name" placeholder="Name" name="name" value={fullName} onChange={({ target }) => setFullName(target.value)} className={SignupCSS.input} required />
+                    </div>
+
+                    <div className={SignupCSS.inputBox}>
+                         <input type="text" id="username" placeholder="Username" name="username" value={username} onChange={({ target }) => setUsername(target.value)} className={SignupCSS.input} />
+                    </div>
+
+                    <div className={SignupCSS.inputBox}>
+                         <input type="email" id="email" placeholder="Email" name="email" value={email} className={SignupCSS.input} onChange={({ target }) => setEmail(target.value)}></input>
+                    </div>
+
+                    <div className={SignupCSS.inputBox}>
+                         <input type="password" id="password" className={SignupCSS.input} name="password" value={password} placeholder="Password" onChange={({ target }) => setPassword(target.value)} />
+                    </div>
+
+                    <button disabled={isInvalid} className={SignupCSS.signup_btn} type="submit" >Sign Up</button>
+
+                    <div className={SignupCSS.already}>
+                         Already have an account? <Link to={"/login"}>Log In</Link>
+                    </div>
+
+                    <div className={SignupCSS.need}><Link to="/">Cancel</Link>
+                    </div>
+
+               </form>
           </div>
      )
 }
